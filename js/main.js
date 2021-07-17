76e975a33d6af8104a7f48fdfb387ce5c1270bbd
kennedy babu <kennedynjogu8@gmail.com>
2021-07-17 22:40:00 +0300

    add day and month validation

--- js/main.js
+++ js/main.js
@@ -2,13 +2,22 @@ const form = document.getElementById("form")
 
 document.getElementById("form").addEventListener("submit", function(e) {
     let userDay = document.getElementById("inputDay").value
+    let userMonth = document.getElementById("inputMonth").value
+    let userYear = document.getElementById("inputYear").value
 
+    let male = document.getElementById("male")
+    let female = document.getElementById("female")
 
     e.preventDefault()
-    userAlert()
-    console.log(userDay, form, form.value)
+
+    if (userDay < 0 || userDay > 31 && userMonth <= 0 || userMonth > 12) {
+        alertUser()
+        return
+    } else {
+        console.log(userDay, userMonth, userYear)
+    }
 })
 
-function userAlert(){
+function alertUser(){
     alert("Enter valid day")
 }

1674ccdf0e2d91185dd16ee8ff04f244c2f8ea1c
kennedy babu <kennedynjogu8@gmail.com>
2021-07-17 22:22:18 +0300

    add checkboxx input

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-var d = document.getElementById('getDay').value
+const form = document.getElementById("form")
 
-console.log(d)
+document.getElementById("form").addEventListener("submit", function(e) {
+    let userDay = document.getElementById("inputDay").value
+
+
+    e.preventDefault()
+    userAlert()
+    console.log(userDay, form, form.value)
+})
+
+function userAlert(){
+    alert("Enter valid day")
+}

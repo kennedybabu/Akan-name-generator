5e81673f5400b2c8dad53df4a67457cb2d75508b
kennedy babu <kennedynjogu8@gmail.com>
2021-07-18 00:41:01 +0300

    display the aknan name for the user

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const form = document.getElementById("form")
 
+maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+femalNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+
 document.getElementById("form").addEventListener("submit", function(e) {
     e.preventDefault()
     let userDay = document.getElementById("inputDay").value
@@ -16,21 +19,25 @@ document.getElementById("form").addEventListener("submit", function(e) {
     var  fourthDigit = yr[3]
 
     var CC = firstDigit + secondDigit
-    var YY = thirdDigit + firstDigit    
+    var YY = thirdDigit + fourthDigit    
+
+    let message = "your Aknan name is "
 
 
     if (userDay < 0 || userDay > 31 && userMonth <= 0 || userMonth > 12) {
         alertUser()
         return
-    } else {
-        console.log(CC, YY)
-    }
-
-    // if( male.checked == true) {
-    //     var birthDay =   ( ( (firstTwo/4) -2*firstTwo-1) + ((5*lastTwo/4) ) + ((26*(MM+1)/10)) + CC ) % 7
-    //     console.log(birthDay)
-    // }
-
+    } else if( male.checked == true) {
+        var birthDay =   ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(userMonth+1)/10)) + userDay ) % 7
+        var key = parseInt(birthDay)
+        const aknanName = maleNames[key]
+        document.getElementById("output").innerHTML = message.concat(aknanName) 
+        
+    } if (female.checked == true) {
+        var birthDay =   ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(userMonth+1)/10)) + userDay ) % 7
+        var key = parseInt(birthDay)
+        console.log(femalNames[key])        
+    } 
 
 })
 

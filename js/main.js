bc00dc241d448abd2885b5936e779bfb509d086c
kennedy babu <kennedynjogu8@gmail.com>
2021-07-17 23:15:07 +0300

    get an array of the year in split in two

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const form = document.getElementById("form")
 
 document.getElementById("form").addEventListener("submit", function(e) {
+    e.preventDefault()
     let userDay = document.getElementById("inputDay").value
     let userMonth = document.getElementById("inputMonth").value
     let userYear = document.getElementById("inputYear").value
@@ -8,14 +9,29 @@ document.getElementById("form").addEventListener("submit", function(e) {
     let male = document.getElementById("male")
     let female = document.getElementById("female")
 
-    e.preventDefault()
+    let yr = userYear.toString()
+    var  firstDigit = yr[0]
+    var  secondDigit = yr[1]
+    var  thirdDigit = yr[2]
+    var  fourthDigit = yr[3]
+
+    var CC = firstDigit + secondDigit
+    var YY = thirdDigit + firstDigit    
+
 
     if (userDay < 0 || userDay > 31 && userMonth <= 0 || userMonth > 12) {
         alertUser()
         return
     } else {
-        console.log(userDay, userMonth, userYear)
+        console.log(CC, YY)
     }
+
+    // if( male.checked == true) {
+    //     var birthDay =   ( ( (firstTwo/4) -2*firstTwo-1) + ((5*lastTwo/4) ) + ((26*(MM+1)/10)) + CC ) % 7
+    //     console.log(birthDay)
+    // }
+
+
 })
 
 function alertUser(){

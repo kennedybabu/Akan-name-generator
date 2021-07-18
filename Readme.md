9931f03ad6e1de141322a550b87498c1d9002462
kennedy babu <kennedynjogu8@gmail.com>
2021-07-18 21:12:06 +0300

    final commit

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Akan Name Generator
+Akan Name Generator is an application that takes your date of birth and gender as user inputs and assigns you an Akan(Ghananian) name depending on the day of the week you were born.
+
+## Installation
+Run the application by copying the url in your browser
+## Technologies used
+HTML
+CSS
+JavaScript
+
+## Contributors
+Kennedy Babu
+
+# License
+[MIT] (https://choosealicense.com/licenses/mit/)
--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 const form = document.getElementById("form")
 
+
+//array of Akan names
 maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 
+
+
 document.getElementById("form").addEventListener("submit", function(e) {
     e.preventDefault()
+    //store user input values
     let userDay = document.getElementById("inputDay").value
     let userMonth = document.getElementById("inputMonth").value
     let userYear = document.getElementById("inputYear").value
@@ -12,6 +17,7 @@ document.getElementById("form").addEventListener("submit", function(e) {
     let male = document.getElementById("male")
     let female = document.getElementById("female")
 
+    
     let yr = userYear.toString()
     var  firstDigit = yr[0]
     var  secondDigit = yr[1]
@@ -23,7 +29,7 @@ document.getElementById("form").addEventListener("submit", function(e) {
 
     let message = "Your Akan name is "
 
-
+    //validate user input
     if (userDay < 0 || userDay > 31 && userMonth <= 0 || userMonth > 12) {
         alertUser()
         return
@@ -31,7 +37,12 @@ document.getElementById("form").addEventListener("submit", function(e) {
         var birthDay =   ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(userMonth+1)/10)) + userDay ) % 7
         var key = parseInt(birthDay)
         var akanName = maleNames[key]
-        document.getElementById("output").innerHTML = message.concat(akanName) 
+        if(akanName === undefined) {
+            alertUserYear()
+        } else {
+            document.getElementById("output").innerHTML = message.concat(akanName) 
+        }
+           
         
     } if (female.checked == true) {
         var birthDay =   ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(userMonth+1)/10)) + userDay ) % 7
@@ -47,8 +58,9 @@ document.getElementById("form").addEventListener("submit", function(e) {
 
 })
 
+//alert functions
 function alertUser(){
-    alert("Enter valid day")
+    alert("Enter valid Date")
 }
 
 function alertUserYear() {
